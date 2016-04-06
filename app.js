7f09968963e07f6d613779aafc744248d0fa1883
Devany Moe <devanymoe@gmail.com>
2016-04-06 16:18:52 -0700

    Update route handlers

--- app.js
+++ app.js
@@ -48,6 +48,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.use(function (req, res, next) {
+  res.locals.user = req.user
+  next()
+})
+
 app.use('/', routes);
 app.use('/users', users);
 
